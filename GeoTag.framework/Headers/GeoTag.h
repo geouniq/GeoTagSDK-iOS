2f4b78a2022cc18cd96b71477347131caa1a420d
paologeouniq <paolo.donato@geouniq.com>
2018-07-31 18:10:16 +0200

    Added framework

---
+++ GeoTag.framework/Headers/GeoTag.h
@@ -0,0 +1,19 @@
+//
+//  GeoTag.h
+//  GeoTag
+//
+//  Created by Paolo Donato on 25/07/18.
+//  Copyright © 2018 Paolo Donato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GeoTag.
+FOUNDATION_EXPORT double GeoTagVersionNumber;
+
+//! Project version string for GeoTag.
+FOUNDATION_EXPORT const unsigned char GeoTagVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeoTag/PublicHeader.h>
+
+
